2a8d1071ab4d609308294486804fed6bb1fdcc94
srinivasasridhar <srinivasa.sridhar@gmail.com>
2013-05-16 17:33:50 +0530

    updated

--- app.js
+++ app.js
@@ -23,6 +23,9 @@ var showNew = function () {
 
     var btnAdd = Ext.getCmp('btnAdd');
     btnAdd.setHidden(true);
+
+    var btnSync = Ext.getCmp('SyncBtn');
+    btnSync.setHidden(true);
 }
 
 var hideNew = function () {
@@ -37,6 +40,9 @@ var hideNew = function () {
 
     var btnBack = Ext.getCmp('btnBack');
     btnBack.setHidden(true);
+
+    var btnSync = Ext.getCmp('SyncBtn');
+    btnSync.setHidden(false);
 
     var btnAdd = Ext.getCmp('btnAdd');
     btnAdd.setHidden(false);
@@ -57,8 +63,13 @@ var showTable = function () {
     db.transaction(function (tx) {
         tx.executeSql('SELECT * FROM tblNCRDetails', [], function (tx, results) {
             var len = results.rows.length, i;
+            
             var list = Ext.getCmp('list');
             list.setHidden(false);
+
+            var btnSync = Ext.getCmp('SyncBtn');
+            if (len > 0) btnSync.setHidden(false); else btnSync.setHidden(true);
+
 
             var tbl = '<table>';
             tbl += '<tr>';
@@ -71,11 +82,11 @@ var showTable = function () {
 
             for (i = 0; i < len; i++) {
                 tbl += '<tr>';
-                tbl += '<td><a href="#" onclick="showEdit(\'' + results.rows.item(i).id + '\')" class="classnameActive">Edit</p></td>';
+                tbl += '<td><a href="#" onclick="showEdit(\'' + results.rows.item(i).id + '\')" class="classnameActive">Edit &nbsp;<img src="edit.png"/></p></td>';
                 tbl += '<td><p>' + results.rows.item(i).subject + '</p></td>';
                 tbl += '<td><p>' + results.rows.item(i).priority + '</p></td>';
                 tbl += '<td><p>' + results.rows.item(i).sbu + '</p></td>';
-                tbl += '<td><a href="#" onclick="deleteRow(\'' + results.rows.item(i).id + '\')" class="classname">Delete</p></td>';
+                tbl += '<td><a href="#" onclick="deleteRow(\'' + results.rows.item(i).id + '\')" class="classname">Delete  &nbsp;<img src="delete.gif"/></p></td>';
                 tbl += '</tr>';
 
             }
@@ -86,13 +97,26 @@ var showTable = function () {
 }
 //End of WebSQL
 
-
 //Sencha Touch --> Start
 Ext.application(
 { viewport: {
     autoMaximize: true
 }, name: 'Incident',
     launch: function () {
+
+    var SyncClick = function()
+                    {
+                    /*
+                    if (Ext.device.Connection.isOnline()) {Ext.Msg.alert('You are currently connected via ' + Ext.device.Connection.getType());} 
+                    else {    Ext.Msg.alert('You are not currently connected');}
+                    */
+                        if (navigator.onLine) {
+                            Ext.Msg.alert('You are currently online');
+                        } else {
+                            Ext.Msg.alert('You are not currently connected to internet');
+                    }
+                    Ext.device.notification.vibrate();
+                    }
 
 
         var formPanel = Ext.create('Ext.form.Panel', {
@@ -103,28 +127,33 @@ Ext.application(
             { xtype: 'titlebar', title: 'Offline NCR (POC)', docked: 'top',
                 items:
                 [
-                { xtype: 'button', id: 'btnAdd', iconMask: true, align: 'right', iconCls: 'add', handler: 
-                function()
-                { 
-                showNew(); 
-                 formPanel.setValues({
-                        subject: '',
-                        desc: '',
-                        priority: '',
-                        sbu: '',
-                        severity: '',
-                        isUpdate: '0'
-                    });
-                }
-                },
+                { xtype: 'button', id: 'btnAdd', iconMask: true, align: 'right', iconCls: 'add', 
+                    handler: 
+                        function()
+                        { 
+                        showNew(); 
+                         formPanel.setValues({
+                                subject: '',
+                                desc: '',
+                                priority: '',
+                                sbu: '',
+                                severity: '',
+                                isUpdate: '0'
+                            });
+                        }
+                    },
+                { html: '<div class="x-button-icon settings x-icon-mask" style="float:left"></div><div style="float:left">Sync</div>', id: 'SyncBtn', handler: SyncClick },
                 { xtype: 'button', id: 'btnBack', iconMask: true, align: 'left', ui: 'back', text: 'Back', handler: hideNew, hidden: true }
+                
                 ]
-            },
-            { id: 'list', html: '<div><center><h1>No Records found.</h1></center></div>', /*showAnimation: 'flip',showAnimation: 'slideIn', hideAnimation: 'slideOut'*/ },
+                },
+            
+            { id: 'list', html: '<div><center><h1>No Records found.</h1></center></div>', showAnimation: 'flip', showAnimation: 'slideIn', hideAnimation: 'slideOut' },
+            
             {
                 xtype: 'fieldset', hidden: 'true', id: 'addNewForm',
-                /*showAnimation: 'flip',
-                hideAnimation: 'flip',*/
+                showAnimation: 'flip',
+                hideAnimation: 'flip',
                 items:
                 [
                  { xtype: 'textfield', name: 'subject', label: 'Subject', placeHolder: 'Subject of NCR', labelWidth: '40%' },
@@ -132,32 +161,44 @@ Ext.application(
                 { xtype: 'selectfield', name: 'priority',
                     label: 'Priority ', labelWidth: '40%',
                     options: [
-                            { text: 'Low', value: '1' },
-                            { text: 'Medium', value: '2' },
-                            { text: 'High', value: '3' }
+                            { text: 'Low', value: 'Low' },
+                            { text: 'Medium', value: 'Medium' },
+                            { text: 'High', value: 'High' }
                             ]
                 },
                 { xtype: 'selectfield', name: 'sbu', id:'sbu',
                     label: 'SBU', labelWidth: '40%',
                     options: [
-                            { text: 'INDIA', value: '1' },
+                            /*{ text: 'INDIA', value: '1' },
                             { text: 'SEA', value: '3' },
                             { text: 'SEOB', value: '5' },
                             { text: 'SETL', value: '6' },
                             { text: 'SWEAS', value: '2' },
                             { text: 'SWECO', value: '4' },
-                            { text: 'SWESA', value: '7' }
-
+                            { text: 'SWESA', value: '7' }*/
+                            {text: 'INDIA', value: 'INDIA' },
+                            { text: 'SEA', value: 'SEA' },
+                            { text: 'SEOB', value: 'SEOB' },
+                            { text: 'SETL', value: 'SETL' },
+                            { text: 'SWEAS', value: 'SWEAS' },
+                            { text: 'SWECO', value: 'SWECO' },
+                            { text: 'SWESA', value: 'SWESA' }
                             ]
                 },
                 { xtype: 'selectfield', name: 'severity', id:'severity',
                     label: 'Severity of Defect', labelWidth: '40%',
                     options: [
+                            /*
                             { text: 'OMS-Commissioning', value: '0' },
                             { text: 'OMS-Service', value: '2' },
                             { text: 'Project-Civil work', value: '3' },
                             { text: 'Project-Commissioning', value: '6' },
-                            { text: 'Project-Installation', value: '7' }
+                            { text: 'Project-Installation', value: '7' }*/
+                              {text: 'OMS-Commissioning', value: 'OMS-Commissioning' },
+                            { text: 'OMS-Service', value: 'OMS-Service' },
+                            { text: 'Project-Civil work', value: 'Project-Civil work' },
+                            { text: 'Project-Commissioning', value: 'Project-Commissioning' },
+                            { text: 'Project-Installation', value: 'Project-Installation' }
                             ]
                 },
                 { xtype: 'textareafield', name: 'desc', label: 'Description', placeHolder: 'Description', labelWidth: '40%' },
@@ -165,7 +206,7 @@ Ext.application(
 
                 ]
             },
-    { xtype: 'panel', items: [{ xtype: 'button', name: 'Submit', text: 'Submit', id: 'btnSubmit', hidden: 'true', /*showAnimation: 'slideIn', hideAnimation: 'slideOut',*/
+    { xtype: 'panel', items: [{ xtype: 'button', name: 'Submit', text: 'Submit', id: 'btnSubmit', hidden: 'true', showAnimation: 'slideIn', hideAnimation: 'slideOut',
         handler: function () {
             var formVals = JSON.stringify(formPanel.getValues(), null, 2);
             var vals = Ext.JSON.decode(formVals);
@@ -210,6 +251,7 @@ Ext.application(
     ]
         });
 
+        /*
         var options = '';
         var sbu = Ext.getCmp('sbu');
         var severity = Ext.getCmp('severity');
@@ -219,9 +261,9 @@ Ext.application(
             options += '{text:' + i + ',value:' + i + '},';
         }
         options = Ext.decode('[' + options+ ']');
-        sbu.setOptions(options);
+                sbu.setOptions(options);
         severity.setOptions(options);
-        
+        */
         showTable();
 
 
